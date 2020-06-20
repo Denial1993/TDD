b7e12c97e03512f44c4b54e6f7329c1e07b551f4
denial <denial821124@gmail.com>
2020-06-20 15:45:43 +0800

    remove the shopping_mall dir  and remove the cats/dogs files

--- hello_rails/db/schema.rb
+++ hello_rails/db/schema.rb
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 20200608064543) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.boolean "is_available"
+    t.integer "store_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products_stores", id: false, force: :cascade do |t|
     t.integer "product_id"
     t.integer "store_id"
